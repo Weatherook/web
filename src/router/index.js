f25cb48c0bad5320aac3716dc1b3c18e1f24f52a
minkyoe <ddal0924@naver.com>
2018-07-02 16:13:16 +0900

    make nav bar, home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomeRoutes from './home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [HomeRoutes],
+  mode: 'history'
 })
