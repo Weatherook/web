59f092d4ceda3d73dd46a0b02a709e880977d56c
minkyoe <ddal0924@naver.com>
2018-07-03 23:42:44 +0900

    edit nav bar, add feed

---
+++ src/pages/FollowingFeed.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>HI</h2>
+</template>
+
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+   
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomeRoutes from './home'
 import MypageRoutes from './mypage'
-
+import TodayFeedRoutes from './todayFeed'
+import FollowingFeedRoutes from './followingFeed'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [HomeRoutes, MypageRoutes],
+  routes: [HomeRoutes, MypageRoutes, TodayFeedRoutes, FollowingFeedRoutes],
   mode: 'history'
 })
