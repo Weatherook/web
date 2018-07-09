40b2bf6cc376d082161a6aa70b77f66bf3ccae9e
EunYeong <povoq1024@gmail.com>
2018-07-09 19:11:40 +0900

    nav view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <template>
   <v-app>
-    <nav v-if="['Home', 'Mypage'].indexOf($route.name) > -1">
+    <nav v-if="['Home', 'Mypage', 'MypageGrid'].indexOf($route.name) > -1">
       <v-layout row wrap>
-        <v-flex row wrap sm6 md5 lg6>
-          <img src="" alt="">
+        <img src="./assets/topimage@3x.png" class="nav-back">
+        <v-flex row sm3 md3 lg3 logos>
+          <img src="./assets/logo_white@2x.png" class="logo_image">
+          <img src="./assets/mainlogo@2x.png" class="main_logo_image">
         </v-flex>
+
+        <v-layout row wrap sm6 md6 lg6 icons>
+          <v-flex column wrap date mt-2>
+            {{ this.mm }} . {{ this.dd }} <br/>
+            {{ this.day }}
+          </v-flex>
+
+          <v-flex ml-4 mt-1>
+            <img :src="this.weatherImage[this.weatherIndex]" class="weather_image">
+          </v-flex>
+
+          <v-flex column wrap date mt-2>
+            25℃ <br/>
+            20 / 28
+          </v-flex>
+
+          <v-flex ml-4>
+            <img src="./assets/topbar_line@2x.png" class="divider_image">
+          </v-flex>
+
+          <v-flex mt-1 ml-2>
+            <v-btn flat icon @click="click_bell">
+              <img src="./assets/bell1@2x.png" class="bell_image">
+            </v-btn>
+          </v-flex>
+
+          <v-flex mt-1 ml-3>
+            <img src="./assets/top-profileface@2x.png" class="profile_image">
+            <v-flex v-if="this.bell_flag" bell-content>
+              <ul>
+                <li>ㅎ</li>
+                <li>ㅅ</li>
+                <li>ㅂ</li>
+              </ul>
+            </v-flex>
+          </v-flex>
+
+        </v-layout>
       </v-layout>
     </nav>
     <router-view/>
@@ -21,7 +61,29 @@ export default {
       toolbarItems: [
         { icon: '', title: '로그인/회원가입', link: '/sign' },
         { icon: 'face', title: '마이페이지', link: '/myPage' }
-      ]
+      ],
+      dd : 0,
+      mm : 0,
+      day : '',
+      week : ['일', '월', '화', '수', '목', '금', '토'],
+      weatherImage : ['../static/weather/sunny@2x.png', '../static/weather/cloud@2x.png', '../static/weather/rainy@2x.png', '../static/weather/snow@2x.png'],
+      weatherIndex : 1,
+      bell_flag : false
+    }
+  },
+  created() {
+    var d = new Date();
+    this.mm = d.getMonth() +1;
+    this.dd = d.getDate();
+    this.day = d.getDay();
+    this.day = this.week[this.day] + "요일";
+  },
+  methods: {
+    click_bell() {
+      this.bell_flag = true;
+      var bell_element = document.getElementsByClassName("bell_image")[0];
+      bell_element.src="../static/home/bell2@2x.png";
+
     }
   }
 }
@@ -31,9 +93,59 @@ export default {
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  
+  -moz-osx-font-smoothing: grayscale; 
 }
 
+.nav-back {
+  width: 100%;
+  height: 100%;
+}
 
+.logos {
+  position: fixed;
+  left: 13%;
+  top: 3%;
+}
+
+.logo_image {
+  width: 80px;
+}
+
+.main_logo_image {
+  width: 100px;
+  margin-bottom:5px;
+}
+
+.icons {
+  position: fixed;
+  right: 8%;
+  top: 7%;
+}
+
+.date {
+  text-align: center;
+}
+
+.weather_image {
+  width: 80%;
+}
+
+.divider_image {
+  width: 70%;
+}
+
+.bell_image {
+  width: 80%;
+}
+
+.profile_image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+}
+
+.bell-content {
+  background: white;
+  z-index: 9999;
+}
 </style>
--- src/pages/MypageGrid.vue
+++ src/pages/MypageGrid.vue
@@ -1,16 +1,12 @@
 <template>
     <v-container grid-list-md > 
-    <!-- v-for="post in posts" :key="post.post_id" -->
+        
         <v-layout row wrap>
-          <v-flex sm6 md6 lg6 v-for = "i in 5" :key= "i"  @click.stop="showFeed = true">
-            <img src="../assets/zezudo.jpg" alt="" class="post_image">
+          <v-flex sm6 md6 lg6 m v-for = "i in 5" :key= "i">
+            <img src="../assets/zezudo.jpg" alt="" class="post_image" @click.stop="showFeed = true">
           </v-flex>
         </v-layout>
-
-        <!-- <FeedDetail v-if="this.showFeed" @close="showFeed = false">
-            <h3 slot="header">custom header</h3>
-        </FeedDetail> -->
-
+        
         <v-dialog v-model="showFeed" max-width="1000" max-height="200" color="white">
             <FeedDetail></FeedDetail>
         </v-dialog>
@@ -35,7 +31,8 @@ export default {
 
 <style>
 .post_image {
-    width : 380px;
+    /* width : 380px; */
+    width: 80%;
     height : auto;
     cursor: pointer;
 }
