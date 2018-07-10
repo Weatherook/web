4df26c9025965a7a66a149cabab1a39d294b7901
EunYeong <povoq1024@gmail.com>
2018-07-10 19:33:24 +0900

    carousel slice

--- src/pages/FollowingDetail.vue
+++ src/pages/FollowingDetail.vue
@@ -3,16 +3,14 @@
         <v-container grid-list-xs fluid>
           <v-layout row wrap>
             <v-flex>
-               <v-carousel>
+               <v-carousel :cycle="false"> 
                     <v-carousel-item v-for="(pages,index) in followingPages" :key="index">
-                        {{ pages }}
-                        <v-layout row wrap>
-                        <v-flex column user-item v-for="(following,i) in followings" :key="i" style="display:inline;">
-                                <img :src="followings[index*24+i].img" class="user_image">
-                                <!-- {{ (pages-1)*24+(following -((pages-1)*24)) }} -->
-                                {{ i }}
+                        <v-layout row wrap following-container>
+                        <v-flex sm2 md2 lg2 column user-item v-for="(following, i) in followings.slice(index*24, index*24+24)" :key="i">
+                                <img :src="following.img" class="user_image">
                                 <v-flex>{{following.name}}</v-flex>
                         </v-flex>
+
                         </v-layout>
                     </v-carousel-item>
                 </v-carousel>
@@ -27,6 +25,7 @@
 export default {
     data() {
         return {
+            aIndex: 0,
             // pages: this.followings.length,
             followings: [ { name:"aaaaaaa", img: "https://pbs.twimg.com/profile_images/895654109700775938/MuR13dsu_400x400.jpg" }, 
                         { name:"aaaaaaa", img: "/static/profileface.png" }, 
@@ -52,25 +51,23 @@ export default {
                         { name:"aaaaaaa", img: "/static/profileface.png" }, 
                         { name:"aaaaaaa", img: "/static/profileface.png" }, 
                         { name:"aaaaaaa", img: "/static/profileface.png" }, 
-                        { name:"aaaaaaa", img: "/static/profileface.png" },
-                        { name:"aaaaaaa", img: "/static/profileface.png" },
-                        { name:"aaaaaaa", img: "/static/profileface.png" },
-                        { name:"aaaaaaa", img: "/static/profileface.png" },
-                        { name:"aaaaaaa", img: "/static/profileface.png" },
-                        { name:"aaaaaaa", img: "/static/profileface.png" }, ] 
+                        { name:"ㅎㅇ", img: "../../static/home/bell1@2x.png" },
+                        { name:"ㅂㅇ", img: "/static/profileface.png" },
+                        { name:"ㅎ", img: "/static/profileface.png" },
+                        { name:"ㅅ", img: "/static/profileface.png" },
+                        { name:"ㅂㅇ", img: "/static/profileface.png" },
+                        { name:"ㅋㅎ", img: "/static/profileface.png" },
+                        { name:"ㅋㅎㅗ", img: "/static/profileface.png" } ] 
         }
     },
     computed: {
         followingPages (){
             var pages = Math.ceil(this.followings.length / 24)
             console.log(pages)
-            // this.pages = pages;
-            // var start = pages 
-            // console.log(start);
             return pages
         },
        
-    }
+    },
 
 }
 </script>
@@ -78,16 +75,19 @@ export default {
 <style>
 
 .user_image{
-    width: 100px;
-    height: 100px;
-    border-radius: 100px;
+    width: 80px;
+    height: 80px;
+    border-radius: 80px;
     display: inline;
 }
 
 .user-item {
     text-align: center;
     font-weight: bold;
-    width: 200px;
+    /* margin: 20px 10px; */
 }
 
+.following-container {
+    text-align: center;
+}
 </style>
