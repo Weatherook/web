10b7ba9384ac4e98d04d9762117ce1b173d41417
EunYeong <povoq1024@gmail.com>
2018-07-14 00:04:40 +0900

    충돌 해겨

--- src/store/store.js
+++ src/store/store.js
@@ -21,11 +21,7 @@ export const store = new Vuex.Store({
     feedComment: [],
     alarm: [],
     other_mypage: null,
-<<<<<<< HEAD
-    mypage_id: ''
-=======
     otherID: ''
->>>>>>> 8a6feedf8f9cada906ed9542a90cab506f2dc661
   },
   mutations: Object.assign({}, Mutations),
   getters: Object.assign({}, mypageGetters, tokenGetters, navGetters, mypageInfoGetters, Getters, FeedCommentGetters, AlarmGetters),
